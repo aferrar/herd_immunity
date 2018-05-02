14b74b7b92eb210fb2a8fdb408b1733369d1652e
aferrar <aferrar90@gmail.com>
2018-05-02 18:31:29 -0400

    Created framework and beginning logic for herd immunity project.

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-var turn_number, mortality_prob, immunity_prob, infect_prob;
+var turnNum, mortalityProb, immunityProb, infectProb;
 
+function randomNumber() {
+    return Math.floor(Math.random() * 100)
+}
+
+function randomInfect() {
+    var randomCell = randomNumber();
+    var x = document.getElementById("population_table").getElementsByTagName("td");
+    x[randomCell].style.background = "Yellow";    
+}
+
+function clearInfect() {
+    var x = document.getElementById("population_table").getElementsByTagName("td");
+    var cell;
+    for (cell=0; cell < 100; cell++) {
+        x[cell].style.background = "Green";   
+    }
+}
