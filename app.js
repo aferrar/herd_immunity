b11e21f895669cf168544ad15a7ffe224fc72f0d
aferrar <aferrar90@gmail.com>
2018-05-09 02:28:13 -0400

    Added functionality for stepping through visualization. Added multiple infection input.

--- app.js
+++ app.js
@@ -34,31 +34,57 @@ function randomVacc(num) {
 UpdateStats();
 }
 
-infected_index = [];
-function infectionStart() {
-        var randomCell = randomNumber();
-        infected_index.push(randomCell.toString());
-        var x = document.getElementById("population_table").getElementsByTagName("td");
-        var y = x[randomCell].classList.item(y)
-        x[randomCell].classList.remove("healthy");
-        x[randomCell].classList.add("infected");
-    
-        // Trying to prevent clicks that don't generate infection color. A little better. Still not // perfect
-    
-        console.log(randomCell);
-        while (y !== "healthy" && randomCell < 20 && infected_index.indexOf(randomCell.toString() !== -1)) {
-            y = x[randomCell].classList.item(y);
-            if (y == "infected" || y == "vaccinated" || y == "dead" || y == "update") {
-                randomCell = randomNumber();
-                y = x[randomCell].classList.item(y);
+function randomInfection(num) {
+    if (num >= 200) {
+        alert("You've entered too many cells. Lower your number of infected cells.")
+    } else {
+        for (var cell = 0; cell < num; cell++) {
+            var randomCell = randomNumber();
+            var x = document.getElementById("population_table").getElementsByTagName("td");
+            var y = x[randomCell].classList.item(y)
+            if (y = "healthy" && randomCell > 20) {
                 x[randomCell].classList.remove("healthy");
                 x[randomCell].classList.add("infected");
-                console.log(y, randomCell);
+            } else {
+                var randomCell = randomNumber();
+                var x = document.getElementById("population_table").getElementsByTagName("td");
+                var y = x[randomCell].classList.item(y)
+                if (y = "healthy" && randomCell > 20) {
+                    x[randomCell].classList.remove("healthy");
+                    x[randomCell].classList.add("infected");
                 }
+            }
         }
-        console.log(y, randomCell);
-        UpdateStats();
+    }
+console.log(y, randomCell);
+UpdateStats();
 }
+
+//infected_index = [];
+//function infectionStart() {
+//        var randomCell = randomNumber();
+//        infected_index.push(randomCell.toString());
+//        var x = document.getElementById("population_table").getElementsByTagName("td");
+//        var y = x[randomCell].classList.item(y)
+//        x[randomCell].classList.remove("healthy");
+//        x[randomCell].classList.add("infected");
+//    
+//        // Trying to prevent clicks that don't generate infection color. A little better. Still not // perfect
+//    
+//        console.log(randomCell);
+//        while (y !== "healthy" && randomCell < 20 && infected_index.indexOf(randomCell.toString() !== -1)) {
+//            y = x[randomCell].classList.item(y);
+//            if (y == "infected" || y == "vaccinated" || y == "dead" || y == "update") {
+//                randomCell = randomNumber();
+//                y = x[randomCell].classList.item(y);
+//                x[randomCell].classList.remove("healthy");
+//                x[randomCell].classList.add("infected");
+//                console.log(y, randomCell);
+//                }
+//        }
+//        console.log(y, randomCell);
+//        UpdateStats();
+//}
 
 function UpdateStats() {
     healthy = 0;
@@ -67,6 +93,7 @@ function UpdateStats() {
     dead = 0;
     var x = document.getElementById("population_table").getElementsByTagName("td");
     var cell;
+    
     for (cell=20; cell<220; cell++) {
         var y = x[cell].classList.item(y);
         if (y == "healthy" || y == "vaccinated") {
@@ -97,12 +124,20 @@ function UpdateStats() {
             x_u[cell].innerHTML= "Turn #: " + turnNumber;
         } else if (cell == 7) {
             x_u[cell].innerHTML = "# \<br />Healthy: " + healthy;
+            x_u[cell].style.fontWeight = "bold";
+            x_u[cell].style.color = "green";
         } else if (cell == 8) {
             x_u[cell].innerHTML = "# Vaccinated: " + vaccinated;
+            x_u[cell].style.fontWeight = "bold";
+            x_u[cell].style.color = "blue";
         } else if (cell == 9) {
             x_u[cell].innerHTML = "# \<br />Infected: " + infected;
+            x_u[cell].style.fontWeight = "bold";
+            x_u[cell].style.color = "red";
         } else if (cell == 10) {
             x_u[cell].innerHTML = "# \<br />Dead: " + dead;
+            x_u[cell].style.fontWeight = "bold";
+            x_u[cell].style.color = "black";
         } else if (cell == 11) {
             x_u[cell].innerHTML = "% Healthy: " + percent_healthy;
             x_u[cell].style.fontWeight = "bold";
@@ -118,7 +153,7 @@ function UpdateStats() {
         } else if (cell == 14) {
             x_u[cell].innerHTML = "% \<br />Dead: " + percent_dead;
             x_u[cell].style.fontWeight = "bold";
-            x_u[cell].style.color = "red";
+            x_u[cell].style.color = "black";
         }
     }
 }
@@ -139,14 +174,14 @@ function infectionSpread() {
                 } else if (199 > cell > 180) {
                     var z1 = x[cell+1].classList.item(z1);
                     var z2 = x[cell-1].classList.item(z2);
-                    var z3 = x[cell+1].classList.item(z3);
+                    var z3 = x[cell+20].classList.item(z3);
                     var z4 = x[cell-20].classList.item(z4);
                 } else if (cell == 199) {
-                    var z1 = x[cell].classList.item(z1);
-                    var z2 = x[cell].classList.item(z2);
-                    var z3 = x[cell].classList.item(z3);
-                    var z4 = x[cell].classList.item(z4);
-                }
+                    var z1 = x[cell+1].classList.item(z1);
+                    var z2 = x[cell-1].classList.item(z2);
+                    var z3 = x[cell+20].classList.item(z3);
+                    var z4 = x[cell-20].classList.item(z4);
+                } 
 
                 if (z1 == "vaccinated") {
                     var probSpread = Math.floor(Math.random() * 10000);
@@ -214,6 +249,96 @@ function infectionSpread() {
         UpdateStats();
     }
 }
+
+function infectionSpreadStep() {
+    var x = document.getElementById("population_table").getElementsByTagName("td");
+    for (var cell = 20; cell < 220; cell++) {
+        var y = x[cell].classList.item(y);
+        // Logic to fix column overflow error which was stalling turn counter
+        if (y == "infected") {
+            if (cell < 180) {
+                var z1 = x[cell+1].classList.item(z1);
+                var z2 = x[cell-1].classList.item(z2);
+                var z3 = x[cell+20].classList.item(z3);
+                var z4 = x[cell-20].classList.item(z4); 
+            } else if (199 > cell > 180) {
+                var z1 = x[cell+1].classList.item(z1);
+                var z2 = x[cell-1].classList.item(z2);
+                var z3 = x[cell+20].classList.item(z3);
+                var z4 = x[cell-20].classList.item(z4);
+            } else if (cell == 199) {
+                var z1 = x[cell+1].classList.item(z1);
+                var z2 = x[cell-1].classList.item(z2);
+                var z3 = x[cell+20].classList.item(z3);
+                var z4 = x[cell-20].classList.item(z4);
+            } 
+
+            if (z1 == "vaccinated") {
+                var probSpread = Math.floor(Math.random() * 10000);
+                if (probSpread < 5 && 220 > (cell + 20)) {
+                    x[cell+1].classList.remove("vaccinated");
+                    x[cell+1].classList.add("infected");
+                }
+
+            } else if (z1 == "healthy") {
+                var probSpread = Math.floor(Math.random() * 10000);
+                if (probSpread < 2500 && 220 > (cell + 20)) {
+                    x[cell+1].classList.remove("healthy");
+                    x[cell+1].classList.add("infected");
+                }
+            }
+
+            if (z2 == "vaccinated") {
+                var probSpread = Math.floor(Math.random() * 10000);
+                if (probSpread < 5) {
+                    x[cell-1].classList.remove("vaccinated");
+                    x[cell-1].classList.add("infected");
+                }
+
+            } else if (z2 == "healthy") {
+                var probSpread = Math.floor(Math.random() * 10000);
+                if (probSpread < 2500) {
+                    x[cell-1].classList.remove("healthy");
+                    x[cell-1].classList.add("infected");
+                }
+            }
+
+            if (z3 == "vaccinated") {
+                var probSpread = Math.floor(Math.random() * 10000);
+                if (probSpread < 5 && 220 > (cell + 20)) {
+                    x[cell+20].classList.remove("vaccinated");
+                    x[cell+20].classList.add("infected");
+                }
+
+            } else if (z3 == "healthy") {
+                var probSpread = Math.floor(Math.random() * 10000);
+                if (probSpread < 2500 && 220 > (cell + 20)) {
+                    x[cell+20].classList.remove("healthy");
+                    x[cell+20].classList.add("infected");
+                }
+            }
+
+            if (z4 == "vaccinated") {
+                var probSpread = Math.floor(Math.random() * 10000);
+                if (probSpread < 5) {
+                    x[cell-20].classList.remove("vaccinated");
+                    x[cell-20].classList.add("infected");
+                }
+
+            } else if (z4 == "healthy") {
+                var probSpread = Math.floor(Math.random() * 10000);
+                if (probSpread < 2500) {
+                    x[cell-20].classList.remove("healthy");
+                    x[cell-20].classList.add("infected");
+                }
+            }
+        }
+    }
+    infectionDeath();
+    turnNumber++;
+    UpdateStats();
+}
+
 
 function infectionDeath() {
     var x = document.getElementById("population_table").getElementsByTagName("td");
